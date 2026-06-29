2de034374b6b57aa9d1ea433da1045cceef8b1b0
muhavan <muhammad400809@gmail.com>
2026-06-30 03:34:08 +0700

    updates

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-// Static portfolio is served entirely from public/index.html.
-// React mounts to a hidden #root and renders nothing.
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  ReactDOM.createRoot(rootEl).render(<></>);
-}
